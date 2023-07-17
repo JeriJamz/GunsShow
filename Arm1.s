b9f6727a52b8a9d9ed9310f3fe6456bb3ab5df54
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-07-17 07:21:19 -0500

    Add files via upload

---
+++ Arm1.s
@@ -0,0 +1,23 @@
+	.text
+	.global start
+start:  ldr sp, =stack_top //gonna remember ldr
+	bl sum //branch link sum(call sum)
+stop:   b  stop//some how this is a loop
+sum: //int sum() will add the total of an array. ion think this it
+	stmfd sp!, {r0-r4, lr}//it does do that
+	mov r0, #0
+	ldr r1, =RESULTS
+	ldr r2, =N //= is apointer
+	ldr r2, [r2]
+loop:	ldr r3, r1 #4//this is anincrementation
+	add r0,r0,r3 //r0 += r3
+	sub r2,r2,r1 //r2-- decrementation
+	cmp r2, #0 //compare(i think)makes it an if. see if we can makewhile loops
+	bne loop //goto spegatti
+	ldr r4, =RESULTS//r4 is loaded
+	str r0,[r4]
+	lmdfdsp!, {r0-r4, pc}
+mal:	ldr r5, #6
+	ldr r6,#4
+	ldr r7, =SUM
+	MULS =SUM,r5,r6
